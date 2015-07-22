4dd985d38d0b248b7720dcc83a79f60b10e212a5
SudarshanReddyG <sudarshang.reddy@gmail.com>
2015-07-22 19:56:54 +0530

    Third Commit

--- src/main/java/com/sudarshan/controller/UserController.java
+++ src/main/java/com/sudarshan/controller/UserController.java
@@ -9,11 +9,15 @@ import java.util.Map;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.sudarshan.model.AppUser;
 import com.sudarshan.service.UserService;
@@ -64,8 +68,64 @@ public class UserController {
 
 		return "users/userform";
 	}
+
+
+	@RequestMapping
+	public String saveOrUpdateUser(@ModelAttribute("userForm") @Validated AppUser userObj, BindingResult result, Model model, RedirectAttributes redirectAttributes) {
+		if(result.hasErrors()) {
+			populateDefaultModel(model);
+			return "users/userform";
+		} else {
+			redirectAttributes.addFlashAttribute("css", "success");
+			if(userObj.isNew()){
+				redirectAttributes.addFlashAttribute("msg", "User added successfully!");
+			}else{
+				redirectAttributes.addFlashAttribute("msg", "User updated successfully!");
+			}
+			userService.saveOrUpdate(userObj);
+
+			return "redirect:/users/" + userObj.getId();
+		}
+	}
 	
-	public String saveOrUpdateUser(@ModelAttribute("userForm"))
+	@RequestMapping(value = "/users/{id}/update", method = RequestMethod.GET)
+	public String showUpdateUserForm(@PathVariable("id") int id, Model model) {
+ 
+		AppUser user = userService.findById(id);
+		model.addAttribute("userForm", user);
+ 
+		populateDefaultModel(model);
+ 
+		return "users/userform";
+ 
+	}	
+	
+	@RequestMapping(value = "/users/{id}/delete", method = RequestMethod.POST)
+	public String deleteUser(@PathVariable("id") int id, 
+		final RedirectAttributes redirectAttributes) {
+ 
+		userService.delete(id);
+ 
+		redirectAttributes.addFlashAttribute("css", "success");
+		redirectAttributes.addFlashAttribute("msg", "User is deleted!");
+ 
+		return "redirect:/users";
+ 
+	}
+	
+	@RequestMapping(value = "/users/{id}", method = RequestMethod.GET)
+	public String showUser(@PathVariable("id") int id, Model model) {
+ 
+		AppUser user = userService.findById(id);
+		if (user == null) {
+			model.addAttribute("css", "danger");
+			model.addAttribute("msg", "User not found");
+		}
+		model.addAttribute("user", user);
+ 
+		return "users/show";
+ 
+	}
 
 	private void populateDefaultModel(Model model) {
 
